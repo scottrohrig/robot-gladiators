0ccaec37116dd1ed6979130d2bdc4afc5de84c5b
Scott Rohrig <scott.rohrig@gmail.com>
2021-11-11 12:18:51 -0800

    add random stats

--- assets/js/game.js
+++ assets/js/game.js
@@ -33,14 +33,16 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
 
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack)
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -59,8 +61,10 @@ var fight = function(enemyName) {
       window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
     }
 
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -76,6 +80,7 @@ var fight = function(enemyName) {
   }
 };
 
+const randomNumber = (min, max) => ( Math.floor( Math.random() * ( max-min + 1 ) ) + min );
 
 // Pseudo code for adding the shop & replay features
 const startGame = function() {
@@ -97,7 +102,8 @@ const startGame = function() {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      // enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60 );
 
       // use debugger to pause script from running and check what's going on at that moment in the code
       // debugger;
@@ -123,7 +129,6 @@ const startGame = function() {
   // while there are still enemies, but the fight is over or fled, ask player to shop
   // if no, skip to the game
   // otherwise show the shop 
-  shop();
 };
 
 
@@ -175,6 +180,9 @@ function shop() {
   switch(shopOptionPrompt) {
 
     // if refill => subtract money and add health
+    // fall-through case: doesn't have a break;
+    case 'Refill':
+    case 'REFILL':
     case 'refill':
       // refill player health
       playerHealth += 20;
